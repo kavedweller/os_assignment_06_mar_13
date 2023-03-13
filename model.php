e2c78f214d1ed591f49fee31246bf3c03fad2198
A Kabir Chowdhury <anisulkabir.c@gmail.com>
2023-03-13 22:06:18 +0600

    added: model.php for task-2

---
+++ model.php
@@ -0,0 +1,44 @@
+<?php
+/*echo '<pre>';
+print_r($_POST);
+print_r($_FILES);
+echo '</pre>';*/
+
+if (isset($_POST['submit_btn']))
+{
+    if ($_POST['name'] && $_POST['password'] && $_FILES['image']['name'] && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && isset($_POST['password']) && isset($_FILES['image']['name']))
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $imageName = $_FILES['image']['name'];
+        $photo = date("Y-m-d-H-i-s") . '-' . $_FILES['image']['name'];
+        $tempLocation = $_FILES['image']['tmp_name'];
+        $directory = 'uploads/';
+
+        move_uploaded_file($tempLocation, $directory . $photo);
+
+        $userData = array($name, $email, $photo);
+
+        echo '<pre>';
+        print_r($userData);
+        echo '</pre>';
+
+        if (file_exists('users.csv'))
+        {
+            $dataFile = fopen('users.csv', 'a');
+        }
+        else
+        {
+            $dataFile = fopen('users.csv', 'w');
+        }
+
+        fputcsv($dataFile, $userData);
+
+        session_start();
+        setcookie('userName', $name);
+    }
+    else
+    {
+        echo('Some information is misssing or invalid');
+    }
+}
